162b247cdae2564cd455431062eca8f0b6c2048c
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2023-04-13 13:08:14 -0300

    Feat: Enable private message events. It just log the messages. I dont know what i will do for common messages and if i will enable common commands

--- src/core/events/index.ts
+++ src/core/events/index.ts
@@ -4,8 +4,8 @@ import { client } from "../client";
 
 import "./commands.event";
 import "./autoComplete.event";
-import "./privateMessage.event";
 import "./guildMessage.event";
+import "./privateMessage.event";
 
 client.once(Events.ClientReady, (instance) => {
   console.log(`Instance Ready! Bot ${instance.user.tag} initialized.`);
--- src/core/events/privateMessage.event.ts
+++ src/core/events/privateMessage.event.ts
@@ -1,11 +1,12 @@
-// import { Events } from "discord.js";
+import { Events } from "discord.js";
 
-// import { client } from "../client";
+import { client } from "../client";
 
-// client.on(Events.MessageCreate, async (interaction) => {
-//   //   console.log('create', interaction)
-// });
-// client.on(Events.MessageUpdate, async (interaction) => {
-//   //if (!interaction.isChatInputCommand()) return;
-//   //   console.log('update', interaction)
-// });
+client.on(Events.MessageCreate, async (message) => {
+  if (message.guildId || message.interaction?.commandName) return;
+  console.log("private_message::create", message);
+});
+client.on(Events.MessageUpdate, async (message) => {
+  if (message.guildId || message.interaction?.commandName) return;
+  console.log("private_message::update", message);
+});
