25747fc1879c8748eb41a18a0321f2b816e5317d
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2025-02-20 09:35:48 -0300

    Create cleanup.command.ts

---
+++ src/commands/cleanup/cleanup.command.ts
@@ -0,0 +1,43 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { SlashCommandBuilder, ChatInputCommandInteraction } from 'discord.js';
+
+
+export default class CleanupCommand {
+  
+    const data = new SlashCommandBuilder()
+      .setName('cleanup')
+      .setDescription('Clear last 100 messages');
+    
+  
+
+  async execute(interaction: ChatInputCommandInteraction) {
+    try {
+      const messages = await interaction.channel?.messages
+        .fetch()
+        .then((data) => data.filter((message) => message.deletable))
+        .then(async (data) => {
+          const len = data.size;
+
+          return {
+            len,
+            promises: data.each(
+              (message) =>
+                new Promise((resolve) => {
+                  message.delete().then(() => resolve(true));
+                }),
+            ),
+          };
+        })
+        .catch((err) => {
+          throw err;
+        });
+      await interaction.reply({
+        content: `${messages?.len} messages included for deleting. Processing it all.`,
+      }),
+        await Promise.all(messages!.promises);
+    } catch (e) {
+      console.error(e);
+      await interaction.reply('fudeu');
+    }
+  }
+}
